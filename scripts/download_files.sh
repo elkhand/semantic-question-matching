6d98826db516a9b62ffab4b2f672643252b93d38
Elkhan Dadashov <elkhan.dadashov@gmail.com>
2019-01-26 23:58:18 -0800

    Removed test.csv which can be downloaded from https://www.kaggle.com/c/quora-question-pairs/download/test.csv
    Former-commit-id: cfff92e5410d970fc2b84e4fa67ba4ac6bfd1c21 [formerly 8221fb5dd21128663e482d14a2319f8ba3204bf5]
    Former-commit-id: d0f5917e4c04a7e86fe4630ac6fec5c0afb3e0b9

--- scripts/download_files.sh
+++ scripts/download_files.sh
@@ -28,6 +28,7 @@ echo `curl -# -C - -o $local_dataset_path $DATASET_URL`
 
 TEST_DATASET_URL="https://storage.googleapis.com/kaggle-competitions-data/kaggle/6277/test.csv.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1548306855&Signature=V4S3wmWK%2BHNAVQLcFtlfNVly1tcxpenTzrIwzAYfxXSUeaOtmEbxXS2H2An8mlSo8zaaS1uLdWOlqH9SmDDW6RUj9gQUz%2FQ9rXlZeYU7oymGCuZ6JvPOUsSqk1vCqusybo4RYnX3mgDSt61lY%2BSmYJnasBV26bgvv%2BJuh%2BV9F29ekzr013bTHP7wUmwKWEgridJoFdb03yrXmS%2Fv9LpnFjxU6F%2FZ20wZ2VTekXNBzl7HLJ5Q4VKqVTcv%2Bagx0HwThSXfQKUNBINSYxW1jyen7pidLpcR2B9X4n43jc9kzZQQRK5IJ2NZbPzWpsLB05g5%2F7aQ%2FDMxCjjQ3RCTj5XKmQ%3D%3D"
 # https://www.kaggle.com/c/quora-question-pairs/data
+# https://www.kaggle.com/c/quora-question-pairs/download/test.csv
 local_test_dataset_path="$DATASET_PATH/test_quora_duplicate_questions.csv.zip"
 echo "Downloading Test Quora Questiosn dataset " $TEST_DATASET_URL " to " $local_test_dataset_path
 echo `curl -# -C - -o $local_test_dataset_path $TEST_DATASET_URL`
